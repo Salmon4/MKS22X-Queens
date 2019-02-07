6075be56b05728ba84011fbaed392c765de3f672
Brandon <bchen00@stuy.edu>
2019-02-07 08:57:53 -0500

    wrote the frame

---
+++ QueenBoard.java
@@ -0,0 +1,15 @@
+public class QueenBoard{
+	private int[][]board;
+	public QueenBoard(int size){
+
+	}
+
+	private boolean addQueen(int r, int c){
+	
+	}
+
+	private boolean removeQueen(int r, int c){
+
+	}
+
+}
